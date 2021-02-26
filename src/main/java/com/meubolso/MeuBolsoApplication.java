5c04672b33ab827dfb44c2b75c1f84be1c01cc65
igoryy <igor.siviero1@gmail.com>
2021-02-25 23:42:05 -0300

    Implementar api

---
+++ src/main/java/com/meubolso/MeuBolsoApplication.java
@@ -0,0 +1,13 @@
+package com.meubolso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuBolsoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuBolsoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/meubolso/representation/Currency.java
@@ -0,0 +1,81 @@
+package com.meubolso.representation;
+
+import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import org.springframework.validation.annotation.Validated;
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+
+/**
+ * Currency
+ */
+@Validated
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2021-02-25T23:11:40.655-03:00")
+
+public class Currency   {
+  @JsonProperty("code")
+  private String code = null;
+
+  public Currency code(String code) {
+    this.code = code;
+    return this;
+  }
+
+  /**
+   * Get code
+   * @return code
+  **/
+  @ApiModelProperty(example = "USD", value = "")
+
+
+  public String getCode() {
+    return code;
+  }
+
+  public void setCode(String code) {
+    this.code = code;
+  }
+
+
+  @Override
+  public boolean equals(java.lang.Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Currency currency = (Currency) o;
+    return Objects.equals(this.code, currency.code);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(code);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class Currency {\n");
+    
+    sb.append("    code: ").append(toIndentedString(code)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(java.lang.Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
+
