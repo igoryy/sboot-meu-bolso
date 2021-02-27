88551530e628ecc1fdc16541bc303f5905576721
igoryy <igor.siviero1@gmail.com>
2021-02-26 23:36:35 -0300

    Implementar mapper currency

--- src/main/java/com/meubolso/repository/CurrencyMapper.java
+++ src/main/java/com/meubolso/repository/CurrencyMapper.java
@@ -3,6 +3,10 @@ package com.meubolso.repository;
 import com.meubolso.currency_api.DataIntegration;
 import com.meubolso.domain.CurrencyDomain;
 import com.meubolso.domain.ListCurrency;
+import com.meubolso.provider.presentation.representation.BTCRepresentation;
+import com.meubolso.provider.presentation.representation.DataRepresentation;
+import com.meubolso.provider.presentation.representation.EURRepresentation;
+import com.meubolso.provider.presentation.representation.USDRepresentation;
 
 public class CurrencyMapper {
 
@@ -38,6 +42,38 @@ public class CurrencyMapper {
 
         return response;
 
+    }
+
+    public static DataRepresentation toRepresentation(DataIntegration dataIntegration){
+
+
+        var btcIntegration = dataIntegration.getBTC();
+        var btcRepresentation = new BTCRepresentation().code(btcIntegration.getCode())
+                .codein(btcIntegration.getCodein())
+                .name(btcIntegration.getName())
+                .high(btcIntegration.getHigh())
+                .low(btcIntegration.getLow());
+
+        var eurIntegration = dataIntegration.getEUR();
+        var eurRepresentation = new EURRepresentation().code(eurIntegration.getCode())
+                .codein(eurIntegration.getCodein())
+                .name(eurIntegration.getName())
+                .high(eurIntegration.getHigh())
+                .low(eurIntegration.getLow());
+
+        var usdIntegration = dataIntegration.getUSD();
+        var usdRepresentation = new USDRepresentation().code(usdIntegration.getCode())
+                .codein(usdIntegration.getCodein())
+                .name(usdIntegration.getName())
+                .high(usdIntegration.getHigh())
+                .low(usdIntegration.getLow());
+
+        var response = new DataRepresentation();
+
+        response.BTC(btcRepresentation).USD(usdRepresentation).EUR(eurRepresentation);
+
+
+        return response;
     }
 
 }
