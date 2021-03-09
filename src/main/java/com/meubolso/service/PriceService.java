a98e607bf2d88173e3496589ac3211299e164104
igoryy <igor.siviero1@gmail.com>
2021-03-09 00:39:19 -0300

    Implementar leitura zip

--- src/main/java/com/meubolso/service/PriceService.java
+++ src/main/java/com/meubolso/service/PriceService.java
@@ -8,14 +8,16 @@ import org.springframework.stereotype.Service;
 import org.springframework.util.FileCopyUtils;
 import org.springframework.util.FileSystemUtils;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.InputStream;
+import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.file.Path;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.Enumeration;
+import java.util.stream.Stream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 
 @Service
 @Slf4j
@@ -39,11 +41,35 @@ public class PriceService {
 
         File file = new File(nameFile);
 
+        readFileInZip(nameFile);
+
         return file.exists() != false ? true : false;
 
     }
 
+    @SneakyThrows
+    public boolean readFileInZip(String nameFile){
 
+
+        ZipFile zipFile = new ZipFile(nameFile);
+        Enumeration<? extends ZipEntry> entries = zipFile.entries();
+
+        while (entries.hasMoreElements()){
+            ZipEntry entry = entries.nextElement();
+
+            System.out.println(zipFile.getInputStream(entry));
+
+            BufferedReader br = new BufferedReader(
+                    new InputStreamReader(zipFile.getInputStream(entry)));
+
+            Stream<String> line = br.lines();
+
+            line.forEach(item -> System.out.println(item));
+        }
+
+        return zipFile != null ? true : false;
+
+    }
 
     @SneakyThrows
     public boolean deleteFilePriceB3(){
--- src/main/java/io/swagger/configuration/Teste.java
+++ src/main/java/io/swagger/configuration/Teste.java
@@ -1,39 +1,18 @@
 package io.swagger.configuration;
 
-import lombok.SneakyThrows;
-
-import org.springframework.util.FileCopyUtils;
-import org.springframework.util.FileSystemUtils;
-
-
-import java.io.FileOutputStream;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.nio.file.Path;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
+import com.meubolso.service.PriceService;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class Teste {
 
 
 
-    @SneakyThrows
+
     public static void main(String[] args) {
 
-        String ZIP = ".zip";
-        LocalDate d1 = LocalDate.now().minusDays(1);
-        String dataD1 = d1.format(DateTimeFormatter.ofPattern("ddMMyyyy"));
+        PriceService service = new PriceService();
+        service.readPriceB3();
 
-        URL url = new URL("http://bvmf.bmfbovespa.com.br/InstDados/SerHist/COTAHIST_D" + dataD1 + ZIP);
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        connection.setRequestMethod("GET");
-
-        InputStream in = connection.getInputStream();
-        FileOutputStream out = new FileOutputStream( "Cotacao" + dataD1 + ZIP);
-        FileCopyUtils.copy(in,out);
-
-        //FileSystemUtils.deleteRecursively(Path.of("test.zip"));
 
     }
 }
